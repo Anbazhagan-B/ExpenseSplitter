69db623cf773e26c5f8bc84fdd2bb3cc07c6a137
Anbazhagan-B <anbazhagan.b414@gmail.com>
2025-03-30 20:48:08 +0530

    Group Expense listing fix

--- src/container/IndividualExpense/individual-expense.js
+++ src/container/IndividualExpense/individual-expense.js
@@ -1,13 +1,11 @@
 import ExpenseList from "../../components/ExpenseList/expenseList.js";
 import FooterComponent from "../Footer/footerComponent.js";
-import { useNavigate } from "react-router-dom";
 import "./individual-expense.css";
 import { useSelector } from "react-redux";
 import useHeaderTitle from "../../hooks/useHeaderTitle.js";
 
 const IndividualExpense = (props) => {
   const { user } = useSelector((state) => state.api);
-  const navigate = useNavigate();
   useHeaderTitle("My Expenses");
   return (
     <div className="individual-expense">
